ed1ef5f56cc68324b97575c365e1847e433d5397
sanisideup <sanidjaya@gmail.com>
2020-01-21 20:39:24 -0800

    create boxes in random spots in relation to rootNode

--- world tracking/world tracking/ViewController.swift
+++ world tracking/world tracking/ViewController.swift
@@ -26,16 +26,44 @@ class ViewController: UIViewController {
         
         // runs configuration of ARWorldTracking
         self.sceneView.session.run(configuration)
+        
+        // create omnidirectional light source into the view
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
 
     @IBAction func Add(_ sender: Any) {
         let node = SCNNode() // node = position in space. no size, shape, or color
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius:0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius:0.03) // 10 cm^3 cube. to create a circle set width = height = length and divide hieght by 2
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.red // set whole node color to red
-        node.position = SCNVector3(0,0,-0.3) // set position of node in relation to rootNode in meters (x,y,z)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white // light reflecting off the surface
+        
+        // randomizes the position between -0.3 and 0.3 for (x,y,z)
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        
+        node.position = SCNVector3(x,y,z) // set position of node in relation to rootNode in meters (x,y,z)
         
         self.sceneView.scene.rootNode.addChildNode(node) // places node on to rootNode
+    }
+    
+    
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause() // stops keeping track of your position and orientation
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in // enumerating through all child nodes of rootNode
+            node.removeFromParentNode() // removing the boxnode from parent node removes it from scene view
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors]) // forget old starting position and orientation and make a new one based on where you are 
+    }
+    
+    // gives you a random number in the range you give it
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
 }
 
