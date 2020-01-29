84ee3662ec52ce1e57db87779d12ccf09fd18266
sanisideup <sanidjaya@gmail.com>
2020-01-29 00:34:34 -0800

    create planets project

---
+++ planets/planets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  planets
+//
+//  Created by Sani Djaya on 1/29/20.
+//  Copyright © 2020 Sani Djaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
