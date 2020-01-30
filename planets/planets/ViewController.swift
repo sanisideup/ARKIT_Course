76ae72d22d84378f2127b02a3f651bc213fe0379
sanisideup <sanidjaya@gmail.com>
2020-01-29 21:32:10 -0800

    add sun, moon, and orbits

--- planets/planets/ViewController.swift
+++ planets/planets/ViewController.swift
@@ -23,23 +23,78 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        let earth = SCNNode()
-        earth.geometry = SCNSphere(radius: 0.2)
-        earth.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "EarthDay") // sets color/texture
-        earth.geometry?.firstMaterial?.specular.contents = UIImage(named: "EarthSpecular") // specular controls how light is reflected
-        earth.geometry?.firstMaterial?.emission.contents = UIImage(named: "EarthClouds") // emission adds or 'emits' the texture to the geometry
-        earth.geometry?.firstMaterial?.normal.contents = UIImage(named: "EarthNormal") // normal adds 3D detail to add mountains/ridges on the surface of the geometry
-        earth.position = SCNVector3(0,0,-0.5)
-        self.sceneView.scene.rootNode.addChildNode(earth)
         
-        let action = SCNAction.rotateBy(x: 0, y: 360.degreeToRadians, z: 0, duration: 2 ) // rotate earth along y axis
-        let forever = SCNAction.repeatForever(action) // loops the action given
-        earth.runAction(forever) // trigger for running the action set just above
+        // define planets
+        let sun = SCNNode(geometry: SCNSphere(radius: 0.35))
+        sun.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Sun")
+        sun.position = SCNVector3(0,0,-1)
+        
+        let earth = planet(geometry: SCNSphere(radius: 0.2), diffuse: UIImage(named: "EarthDay")!, specular: UIImage(named: "EarthSpecular"), emission: UIImage(named: "EarthClouds")!, normal: UIImage(named: "EarthNormal")!, position: SCNVector3(2,0,0))
+        let moon = planet(geometry: SCNSphere(radius: 0.04), diffuse: UIImage(named: "Moon")!, specular: nil, emission: nil, normal: nil, position: SCNVector3(0,0,0.3))
+        
+        let venus = planet(geometry: SCNSphere(radius: 0.1), diffuse: UIImage(named: "VenusSurface")!, specular: nil, emission: UIImage(named: "VenusAtmosphere")!, normal: nil, position: SCNVector3(0.65,0,0))
+        
+        // define nodes to be used for orbit rotation
+        let earthOrbit = SCNNode()
+        let moonOrbit = SCNNode()
+        let venusOrbit = SCNNode()
+        earthOrbit.position = SCNVector3(0,0,-1)
+        moonOrbit.position = SCNVector3(2,0,0)
+        venusOrbit.position = SCNVector3(0,0,-1)
+
+        
+        // place nodes
+        self.sceneView.scene.rootNode.addChildNode(sun)
+        self.sceneView.scene.rootNode.addChildNode(earthOrbit)
+        self.sceneView.scene.rootNode.addChildNode(venusOrbit)
+        earthOrbit.addChildNode(earth)
+        earthOrbit.addChildNode(moonOrbit)
+        moonOrbit.addChildNode(moon)
+//        venusOrbit.addChildNode(venus)
+        
+        // planet/sun rotation actions
+        let sunRotationAction = rotateForever(duration: 16)
+        sun.runAction(sunRotationAction)
+        
+        let earthRotationAction = rotateForever(duration: 8)
+        let moonRotationAction = rotateForever(duration: 8)
+        earth.runAction(earthRotationAction)
+        moon.runAction(moonRotationAction)
+        
+        let venusRotationAction = rotateForever(duration: 2)
+        venus.runAction(venusRotationAction)
+        
+        // orbit actions
+        let earthOrbitAction = rotateForever(duration: 20)
+        let moonOrbitAction = rotateForever(duration: 2)
+        earthOrbit.runAction(earthOrbitAction)
+        moonOrbit.runAction(moonOrbitAction)
+        
+        let venusOrbitAction = rotateForever(duration: 10)
+        venusOrbit.runAction(venusOrbitAction)
+
+    }
+    
+    func planet(geometry: SCNGeometry, diffuse: UIImage, specular: UIImage?, emission: UIImage?, normal: UIImage?, position: SCNVector3) -> SCNNode {
+        let node = SCNNode(geometry: geometry)
+        node.geometry?.firstMaterial?.diffuse.contents = diffuse // sets color/texture
+        node.geometry?.firstMaterial?.specular.contents = specular// specular controls how light is reflected
+        node.geometry?.firstMaterial?.emission.contents = emission // emission adds or 'emits' the texture to the geometry
+        node.geometry?.firstMaterial?.normal.contents = normal // normal adds 3D detail to add mountains/ridges on the surface of the geometry
+        node.position = position
+        return node
+    }
+    
+    // rotate forever function
+    func rotateForever(duration: Int) -> SCNAction {
+        let rotationAction = SCNAction.rotateBy(x: 0, y: 360.degreeToRadians, z: 0, duration: TimeInterval(duration))
+        return SCNAction.repeatForever(rotationAction) // loop action forever
     }
 
 
 }
 
+// convert degrees to radians
 extension Int {
     var degreeToRadians: CGFloat { return CGFloat(Double(self) * .pi/180) }
     
