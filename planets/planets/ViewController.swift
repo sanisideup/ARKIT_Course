a1f64630abb2bf98053c25c63b25f89f5102648f
sanisideup <sanidjaya@gmail.com>
2020-01-29 19:58:53 -0800

    complete earth

--- planets/planets/ViewController.swift
+++ planets/planets/ViewController.swift
@@ -16,21 +16,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+//        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.session.run(configuration)
-        
+        self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         let earth = SCNNode()
         earth.geometry = SCNSphere(radius: 0.2)
-        earth.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "EarthDay")
-        earth.position = SCNVector3(0,0,-0.4)
+        earth.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "EarthDay") // sets color/texture
+        earth.geometry?.firstMaterial?.specular.contents = UIImage(named: "EarthSpecular") // specular controls how light is reflected
+        earth.geometry?.firstMaterial?.emission.contents = UIImage(named: "EarthClouds") // emission adds or 'emits' the texture to the geometry
+        earth.geometry?.firstMaterial?.normal.contents = UIImage(named: "EarthNormal") // normal adds 3D detail to add mountains/ridges on the surface of the geometry
+        earth.position = SCNVector3(0,0,-0.5)
         self.sceneView.scene.rootNode.addChildNode(earth)
         
-        let action = SCNAction.rotateBy(x: 0, y: 360.degreeToRadians, z: 0, duration: 8) // rotate earth along y axis for 8 seconds
-        earth.runAction(action) // trigger for running the action set just above
+        let action = SCNAction.rotateBy(x: 0, y: 360.degreeToRadians, z: 0, duration: 2 ) // rotate earth along y axis
+        let forever = SCNAction.repeatForever(action) // loops the action given
+        earth.runAction(forever) // trigger for running the action set just above
     }
 
 
