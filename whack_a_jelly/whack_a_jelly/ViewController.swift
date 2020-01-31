d7d425dc842b1595ebf441eddb6f0810d796554a
sanisideup <sanidjaya@gmail.com>
2020-01-30 19:10:05 -0800

    add jellyfish

--- whack_a_jelly/whack_a_jelly/ViewController.swift
+++ whack_a_jelly/whack_a_jelly/ViewController.swift
@@ -19,13 +19,41 @@ class ViewController: UIViewController {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.sceneView.session.run(configuration)
         
-        // Do any additional setup after loading the view.
+        // trigger to identify that the sceneView was tapped
+        // when tapGestureRecognizer is identified it runs the function handleTap
+        let tapGestureReocgnizer = UITapGestureRecognizer(target:self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureReocgnizer)
+ 
     }
 
     @IBAction func play(_ sender: Any) {
+        self.addNode()
     }
     
     @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode() {
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "JellyFish", recursively: false)
+        jellyFishNode!.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(jellyFishNode!)
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) { // sender: UITapGestureRecognizer sends information about the tap
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates) // gets the the information about what you hit
+        
+        if hitTest.isEmpty {
+            print("didn't touch anything")
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry)
+        }
+            
+
     }
 }
 
