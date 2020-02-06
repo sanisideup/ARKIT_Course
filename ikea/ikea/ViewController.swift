d6d7dc9dadedebc513fce3258053ba7c745d3ab4
sanisideup <sanidjaya@gmail.com>
2020-02-05 22:45:07 -0800

    add ability to place objects onto horizontal planes

--- ikea/ikea/ViewController.swift
+++ ikea/ikea/ViewController.swift
@@ -14,20 +14,52 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var itemsCollectionView: UICollectionView!
     let configuraiton = ARWorldTrackingConfiguration()
-    let itemsArray: [String] = ["cup", "vase", "bag", "table"]
+    let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+    var selectedItem: String? // set as optional string to allow conditional below in func addItem
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuraiton.planeDetection = .horizontal // detect horizontal planes!
         self.sceneView.session.run(configuraiton)
         self.itemsCollectionView.dataSource = self
         self.itemsCollectionView.delegate = self
-        
+        self.registerGestureRecognizer()
         // Do any additional setup after loading the view.
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func registerGestureRecognizer() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer) // when a tap gesture is recognized on sceneview the tapped function gets called
+    }
+    
+    @objc func tapped(sender: UITapGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView   // we know that the sceneview sent is ARSCNView, so force type
+        let tapLocation = sender.location(in: sceneView) // get location of user tap location in the sceneview
+        let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent) // checks if tapLocation is a plane identified (in this case horizontal plane)
+        
+        // if true, then touched identified horizontal plane
+        if !hitTest.isEmpty {
+            self.addItem(hitTestResult: hitTest.first!)
+        } else {
+            print("boo")
+        }
+    }
+    
+    func addItem(hitTestResult: ARHitTestResult) {
+        if let selectedItem = self.selectedItem { // if selected item is not nil, then place selected item
+            let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn") // place model with name from selectedItem (defined in itemsArray)
+            let node = scene?.rootNode.childNode(withName: selectedItem, recursively: false)
+            let transform = hitTestResult.worldTransform // get transform matrix of hitTestResult
+            let thirdCol = transform.columns.3 // get the third column of the transform matrix to get the position of the horizontal plane detected
+            node?.position = SCNVector3(thirdCol.x,thirdCol.y,thirdCol.z)
+            self.sceneView.scene.rootNode.addChildNode(node!)
+            
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { // returns how many cells to display in the collection view
@@ -42,12 +74,15 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) { //gets triggered when you select a certain cell
         let cell = collectionView.cellForItem(at: indexPath) //
+        self.selectedItem = itemsArray[indexPath.row] // get name of selected item from items array
         cell?.backgroundColor = UIColor.green // set color selected as green
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) { // gets triggered when you deselect a cell
         let cell = collectionView.cellForItem(at: indexPath) //
-        cell?.backgroundColor = UIColor.blue // set color selected as green
+        cell?.backgroundColor = UIColor.orange // set color unselected
     }
+    
+    
 }
 
