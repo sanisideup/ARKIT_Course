39a01d5187758e449245390149dc9e007f74ff0b
sanisideup <sanidjaya@gmail.com>
2020-02-06 17:47:43 -0800

    add ability to resize and rotate items

--- ikea/ikea/ViewController.swift
+++ ikea/ikea/ViewController.swift
@@ -9,22 +9,25 @@
 import UIKit
 import ARKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var itemsCollectionView: UICollectionView!
+    @IBOutlet weak var planeDetected: UILabel!
     let configuraiton = ARWorldTrackingConfiguration()
     let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
     var selectedItem: String? // set as optional string to allow conditional below in func addItem
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+//        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.configuraiton.planeDetection = .horizontal // detect horizontal planes!
         self.sceneView.session.run(configuraiton)
         self.itemsCollectionView.dataSource = self
         self.itemsCollectionView.delegate = self
         self.registerGestureRecognizer()
+        self.sceneView.delegate = self
+        self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view.
     }
 
@@ -34,19 +37,56 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func registerGestureRecognizer() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(rotate))
         self.sceneView.addGestureRecognizer(tapGestureRecognizer) // when a tap gesture is recognized on sceneview the tapped function gets called
+        self.sceneView.addGestureRecognizer(pinchGestureRecognizer)
+        longPressGestureRecognizer.minimumPressDuration = 0.1 
+        self.sceneView.addGestureRecognizer(longPressGestureRecognizer)
     }
     
     @objc func tapped(sender: UITapGestureRecognizer) {
-        let sceneView = sender.view as! ARSCNView   // we know that the sceneview sent is ARSCNView, so force type
+        let sceneView = sender.view as! ARSCNView   // add tap gesture recognizer to scene view
         let tapLocation = sender.location(in: sceneView) // get location of user tap location in the sceneview
         let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent) // checks if tapLocation is a plane identified (in this case horizontal plane)
         
-        // if true, then touched identified horizontal plane
+        // if hitTest is not empty, then user touched identified horizontal plane
         if !hitTest.isEmpty {
             self.addItem(hitTestResult: hitTest.first!)
         } else {
             print("boo")
+        }
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView // add pinch gesture recognizer to sceneview
+        let pinchLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(pinchLocation)
+        
+        if !hitTest.isEmpty {
+            let results = hitTest.first!
+            let node = results.node
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0) // define scale action that is based on how much you pinch the screen
+            node.runAction(pinchAction)
+            sender.scale = 1.0
+        }
+    }
+    
+    @objc func rotate(sender: UILongPressGestureRecognizer) {
+        let sceneView = sender.view as! ARSCNView
+        let holdLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(holdLocation)
+        
+        if !hitTest.isEmpty {
+            let results = hitTest.first!
+            let node = results.node
+            if sender.state == .began {
+                let rotate = SCNAction.rotateBy(x: 0, y: 360.degreeToRadians, z: 0, duration: 1)
+                let forever = SCNAction.repeatForever(rotate)
+                node.runAction(forever)
+            } else if sender.state == .ended {
+                node.removeAllActions()
+            }
         }
     }
     
@@ -57,6 +97,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             let transform = hitTestResult.worldTransform // get transform matrix of hitTestResult
             let thirdCol = transform.columns.3 // get the third column of the transform matrix to get the position of the horizontal plane detected
             node?.position = SCNVector3(thirdCol.x,thirdCol.y,thirdCol.z)
+            if selectedItem == "table" {
+                self.centerPivot(for: node!)
+            }
             self.sceneView.scene.rootNode.addChildNode(node!)
             
         }
@@ -83,6 +126,28 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         cell?.backgroundColor = UIColor.orange // set color unselected
     }
     
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return} // check if ARPlaneAnchor is added, else exit function
+        DispatchQueue.main.async {
+            self.planeDetected.isHidden = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) { // after 3 seconds hide label
+                self.planeDetected.isHidden = true
+            }
+        }
+    }
     
+    func centerPivot(for node: SCNNode) {
+        let min = node.boundingBox.min
+        let max = node.boundingBox.max
+        node.pivot = SCNMatrix4MakeTranslation(
+            min.x + (max.x - min.x)/2,
+            min.y + (max.y - min.y)/2,
+            min.z + (max.z - min.z)/2)
+    }
 }
+    
 
+// convert degrees to radians
+extension Int {
+    var degreeToRadians: CGFloat { return CGFloat(Double(self) * .pi/180) }
+}
